e4e838dc7a667abc3ed30e4632ddca0cf8245cd6
Itachi0906 <125734169+Itachi0906@users.noreply.github.com>
2025-12-19 22:43:40 +0530

    Add ImGUI project configuration to premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,30 @@
+project "ImGUI"
+    Kind "StaticLib"
+    Language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On" --staticly linking the runtime libraries
+
+    filter {system:windows, configurations:Release}
+        buildoptions "/MT"
 
